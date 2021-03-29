3062ea76d74d99e1ae5ed09b3cbb5083087aa195
Nastasiia-Smr <volhvina@outlook.com>
2021-03-29 23:43:20 +0200

    update game screens
    added pokemon images
    
    update choose pokemon screen & map to pokemon objects

--- PB-pokemon-lite/js/game.js
+++ PB-pokemon-lite/js/game.js
@@ -1,3 +1,19 @@
+class Battle {
+  constructor() {
+    this.turn = 0;
+    this.currentPlayerTurn;
+    this.players = [];
+  }
+}
+
+class Player {
+  constructor(no) {
+    this.no = no;
+    this.pokemon;
+    this.pokemonImage;
+  }
+}
+
 // Define game state, this state is mapped to div id
 const gamestate = {
   INTRO: "intro",
@@ -6,41 +22,89 @@ const gamestate = {
   GAMEOVER: "gameover"
 };
 
-var currentGamestate;
+// API pokemon image
+const pokemonApi = "https://pokeapi.co/api/v2/pokemon/";
 
+// Game variable
+var currentGamestate;
+var player1;
+var player2;
+var currentPlayerToChoosePokemon;
+
+// Game-flow functions
 // Initialize the game, set gamestate to intro
 function onInit() {
-  currentGamestate = gamestate.INTRO;
-  setView(currentGamestate);
+  initGame();
+  setActiveView(currentGamestate);
 }
 
 // Start the game and set gamestate to choose
 function onStart() {
+  initChoose();
   currentGamestate = gamestate.CHOOSE;
-  setView(currentGamestate);
+  setActiveView(currentGamestate);
 }
 
 // Start the battle
 function onStartBattle() {
   currentGamestate = gamestate.BATTLE;
-  setView(currentGamestate);
+  setActiveView(currentGamestate);
 }
 
 // After gameover, this function can be called to restart the game
-// basically reset the gamestate into intro
+// reset the gamestate to intro and reset other variables
 function onPlayAgain() {
-  currentGamestate = gamestate.INTRO;
-  setView(currentGamestate);
+  initGame();
+  setActiveView(currentGamestate);
 }
 
 // function to set the view based on gamestate
-function setView(currentGamestate) {
+function setActiveView(currentGamestate) {
   for (var state in gamestate) {
     if (gamestate[state] === currentGamestate) {
       document.getElementById(gamestate[state]).style.display = "block";
     } else {
       document.getElementById(gamestate[state]).style.display = "none";
     }
-    console.log("Set view: " + currentGamestate);
+    console.log("Set active view: " + currentGamestate);
   }
+}
+
+// Initialize game state
+function initGame() {
+  currentGamestate = gamestate.INTRO;
+  player1 = new Player(1);
+  player2 = new Player(2);
+  currentPlayerToChoosePokemon = {};
+}
+
+// Initializing choose screen
+function initChoose() {
+  currentPlayerToChoosePokemon = player1;
+
+  document.getElementById("choose-pokemon-list").innerHTML = "";
+  let pokemonHtmlButton = "";
+  pokemons.forEach(pokemon => {
+    pokemonHtmlButton += `<button type="button" class="nes-btn" style="padding:0;" data-pokemon="${pokemon.pokemonName}" onclick="choosed(this)"><img
+  src="/img/pokemon/${pokemon.pokemonName}.png"></button>`;
+  })
+  document.getElementById("choose-pokemon-list").innerHTML = pokemonHtmlButton;
+}
+
+// Function for choosing pokemon for p1 & p2
+function choosed(event) {
+  if (currentPlayerToChoosePokemon === player1) {
+    player1.pokemon = clonePokemon(pokemons.find(x => x.pokemonName === event.dataset.pokemon));
+    currentPlayerToChoosePokemon = player2;
+    document.getElementById("choose-player-name").innerHTML = "Player " + player2.no;
+  } else if (currentPlayerToChoosePokemon === player2) {
+    player2.pokemon = clonePokemon(pokemons.find(x => x.pokemonName === event.dataset.pokemon));
+    onStartBattle();
+  }
+}
+
+// We need to clone pokemon before sending it to battle
+// In case it died, we still have the original ^^ wink wink
+function clonePokemon(pokemon) {
+  return JSON.parse(JSON.stringify(pokemon));
 }
