00da64b36eb1d72ad20d92e2dacb9549e29c9fc2
Brisilda Bushi <b.brisilda1552@gmail.com>
2021-03-29 14:20:12 +0200

    Finishing with fuctions

--- PB-pokemon-lite/index.js
+++ PB-pokemon-lite/index.js
@@ -15,15 +15,31 @@ class Pokemon {
     this.totalMagic=totalMagic;
 }
 
-  learnAttackSkill (skill) {
+    learnAttackSkill (skill) {
     this.skillsVariety.push(skill)
     }
   
-  attack (index, attackedPokemon) {
-    this.skillsVariety[index].
-  }
+    attack(index, attackedPokemon) {
+      attackedPokemon.totalHealth-=this.skillsVariety[index].attackPower;
+      this.totalMagic-=this.skillsVariety[index].magicConsume;
+      console.log(`${this.pokemonName} launched skill ${this.skillsVariety[index].skillName} successfully`)
+      console.log(`${attackedPokemon.pokemonName} got ${this.skillsVariety[index].attackPower} damage`)
+    }
+
+    showStatus () {
+      console.log(`${this.pokemonName} status: 
+     \t - health: ${this.totalHealth}
+     \t - magic: ${this.totalMagic}`);
+    }
+
+    getMagic() {
+      this.totalMagic += Math.floor(Math.random() * 50) + 10;
+    }
+
 
 }
+
+
 
 class AttackSkill {
     constructor(skillName, attackPower, magicConsume) {
@@ -37,14 +53,26 @@ class AttackSkill {
 let pikachu = new Pokemon ("Pikachu", 100, 60);
 let lightning = new AttackSkill("lightning", 40, 30); 
 pikachu.learnAttackSkill(lightning);
-console.log(pikachu);
+// console.log(pikachu);
 
-let bulbasaur = new Pokemon("bulbasaur", 95, 105);
+let bulbasaur = new Pokemon("Bulbasaur", 95, 105);
 let poisonSeed = new AttackSkill ("poison seed", 20, 20);
 bulbasaur.learnAttackSkill(poisonSeed);
-console.log(bulbasaur);
+// console.log(bulbasaur);
 
-pikachu.attack(0, bulbasaur);
+pikachu.showStatus();
+pikachu.getMagic();
+// pikachu.attack(0, bulbasaur);
+// bulbasaur.attack(0, pikachu);
+// pikachu.showStatus();
+// bulbasaur.showStatus();
+// pikachu.showStatus();
+// bulbasaur.showStatus();
+// pikachu.attack(0, bulbasaur);
+// pikachu.attack(0, bulbasaur);
+// pikachu.getMagic();
+// pikachu.attack(0, bulbasaur);
+// bulbasaur.attack(0, pikachu);
 
 
 
