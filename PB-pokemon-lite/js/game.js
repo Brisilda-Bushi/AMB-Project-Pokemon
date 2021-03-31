e35d2435b72a79eba456aff9ecd2538e3cd22f59
Nastasiia-Smr <volhvina@outlook.com>
2021-04-01 00:04:04 +0200

    add audio
    add battle animation
    
    add pre battle "fight button", this button also start the battle theme

--- PB-pokemon-lite/js/game.js
+++ PB-pokemon-lite/js/game.js
@@ -13,6 +13,36 @@ class Player {
   }
 }
 
+class AudioPlayer {
+  constructor(audio) {
+    this.audio = audio;
+    this.audio.volume = 0.3;
+    this.battleThemes = ["audio/battle1.mp3"];
+    this.victoryThemes = ["audio/victory1.mp3"];
+    this.introThemes = ["audio/intro1.mp3"];
+    this.chooseThemes = [""]
+  }
+
+  playIntroTheme(){
+    this.audio.src = this.introThemes[0];
+    this.audio.play();
+  }
+
+  playBattleTheme() {
+    this.audio.src = this.battleThemes[0];
+    this.audio.play();
+  }
+
+  playVictoryTheme() {
+    this.audio.src = this.victoryThemes[0];
+    this.audio.play();
+  }
+
+  stop() {
+    this.audio.pause();
+  }
+}
+
 // Define game state, this state is mapped to div id
 const gamestate = {
   INTRO: "intro",
@@ -23,7 +53,9 @@ const gamestate = {
 
 // Intro animation
 let introAnimationGif = new Image();
-introAnimationGif.src = "/img/intro-no-loop-compressed.gif";
+introAnimationGif.src = "img/intro-no-loop-compressed.gif";
+
+let audioPlayer;
 
 // Game variable
 let currentGamestate;
@@ -33,19 +65,22 @@ let battle;
 
 // Game-flow functions
 // Initialize the game, set gamestate to intro
-function onInit() {
+function showIntro() {
+  audioPlayer = new AudioPlayer(document.getElementById("audio"));
+  audioPlayer.stop();
+  audioPlayer.playIntroTheme();
   initGame();
   setActiveView(currentGamestate);
 }
 
 // Start the game and set gamestate to choose
-function onStartGame() {
+function showChoose() {
   initChoose();
   setActiveView(currentGamestate);
 }
 
 // Start the battle
-function onStartBattle() {
+function showBattle() {
   initBattle();
   updateBattle();
   setActiveView(currentGamestate);
@@ -53,7 +88,7 @@ function onStartBattle() {
 
 // After gameover, this function can be called to restart the game
 // reset the gamestate to intro and reset other variables
-function onPlayAgain() {
+function showGameover() {
   initGame();
   setActiveView(currentGamestate);
 }
@@ -91,7 +126,7 @@ function initChoose() {
   let pokemonHtmlButton = "";
   pokemons.forEach(pokemon => {
     pokemonHtmlButton += `<button type="button" class="nes-btn" style="padding:0;" data-pokemon="${pokemon.pokemonName}" onclick="choosed(this)"><img
-  src="/img/pokemon/${pokemon.pokemonName}.png"></button>`;
+  src="img/pokemon/${pokemon.pokemonName}.png"></button>`;
   })
   document.getElementById("choose-pokemon-list").innerHTML = pokemonHtmlButton;
 
@@ -104,6 +139,7 @@ function initBattle() {
   battle.currentPlayer = player1;
   battle.targetPlayer = player2;
   currentGamestate = gamestate.BATTLE;
+  showBattleMenu(false);
 }
 
 // Function for choosing pokemon for p1 & p2
@@ -115,7 +151,7 @@ function choosed(event) {
     document.getElementById("choose-pokemon-container").classList.add("player2-offset");
   } else if (currentPlayerToChoosePokemon === player2) {
     player2.pokemon = clonePokemon(pokemons.find(x => x.pokemonName === event.dataset.pokemon));
-    onStartBattle();
+    showBattle();
   }
 }
 
@@ -132,10 +168,15 @@ function clonePokemon(pokemon) {
   return clonePokemon;
 }
 
+function startBattle() {
+  audioPlayer.playBattleTheme();
+  showBattleMenu(true);
+}
+
 // This function update the pokemon status on the html based on the object state
 function updateBattle() {
   // update current player pokemon image
-  document.getElementById("current-player-pokemon").src = `/img/pokemon/back/${battle.currentPlayer.pokemon.pokemonName}.png`;
+  document.getElementById("current-player-pokemon").src = `img/pokemon/back/${battle.currentPlayer.pokemon.pokemonName}.png`;
 
   // update current player status
   document.getElementById("current-player-no").innerHTML = "Player " + battle.currentPlayer.no;
@@ -151,7 +192,7 @@ function updateBattle() {
 
 
   // update target player pokemon image
-  document.getElementById("target-player-pokemon").src = `/img/pokemon/${battle.targetPlayer.pokemon.pokemonName}.png`;
+  document.getElementById("target-player-pokemon").src = `img/pokemon/${battle.targetPlayer.pokemon.pokemonName}.png`;
   document.getElementById("target-player-no").innerHTML = "Player " + battle.targetPlayer.no;
   document.getElementById("target-player-pokemon-name").innerHTML = battle.targetPlayer.pokemon.pokemonName;
   document.getElementById("target-player-pokemon-current-health").innerHTML = battle.targetPlayer.pokemon.currentHealth;
@@ -166,18 +207,47 @@ function updateBattle() {
 
 // Battle command function which is binded to attack and boost button
 function battleCommand(command) {
+  enableCommandButton(false);
   if (battle.currentPlayer.pokemon.skillsVariety.length > 0) {
     if (command === 'attack') {
-      attackCommand(battle.currentPlayer, battle.targetPlayer);
+      const success = attackCommand(battle.currentPlayer, battle.targetPlayer);
+      if (success) {
+        document.getElementById("current-player-pokemon").classList.add("animation-attack");
+        document.getElementById("target-player-pokemon").classList.add("animation-damage");
+      }
     } else if (command === 'boost') {
       getMagicCommand(battle.currentPlayer);
     }
 
-    // swap player
-    const temp = battle.currentPlayer;
-    battle.currentPlayer = battle.targetPlayer;
-    battle.targetPlayer = temp;
-    updateBattle();
+    // set delay for battle animation
+    setTimeout(() => {
+      // check winning condition
+      // if target pokemon health is less than 0 or equal 0 then gameover
+      if (battle.targetPlayer.pokemon.currentHealth <= 0) {
+        audioPlayer.stop();
+        audioPlayer.playVictoryTheme();
+        const pokemonDeadMessage = `${battle.targetPlayer.pokemon.pokemonName} is defeated`;
+        document.getElementById("battle-log").innerHTML = pokemonDeadMessage;
+        const winningMessage = `Player ${battle.currentPlayer.no} ${battle.currentPlayer.pokemon.pokemonName} win`;
+        document.getElementById("game-over-player-win").innerHTML = winningMessage;
+
+        // set delay to switch screen to gameover
+        setTimeout(() => {
+          currentGamestate = gamestate.GAMEOVER;
+          setActiveView(currentGamestate);
+        }, 3000);
+        enableCommandButton(false);
+      } else {
+        const temp = battle.currentPlayer;
+        battle.currentPlayer = battle.targetPlayer;
+        battle.targetPlayer = temp;
+        enableCommandButton(true);
+      }
+
+      document.getElementById("current-player-pokemon").classList.remove("animation-attack");
+      document.getElementById("target-player-pokemon").classList.remove("animation-damage");
+      updateBattle();
+    }, 1000);
   } else {
     let errorMessage = `current player pokemon:${battle.currentPlayer.pokemon.pokemonName} doesnt have any skill, please initialize the pokemon correctly`;
     alert(errorMessage);
@@ -185,6 +255,32 @@ function battleCommand(command) {
   }
 
   battle.turn++;
+}
+
+function enableCommandButton(enable) {
+  if (enable) {
+    document.getElementById("attack-btn").classList.remove("is-disabled");
+    document.getElementById("boost-btn").classList.remove("is-disabled");
+  } else {
+    document.getElementById("attack-btn").classList.add("is-disabled");;
+    document.getElementById("boost-btn").classList.add("is-disabled");
+  }
+}
+
+function showBattleMenu(show) {
+  if (show) {
+    document.getElementById("current-player-pokemon-status").style.display = "block";
+    document.getElementById("target-player-pokemon-status").style.display = "block";
+    document.getElementById("attack-btn").style.display = "block";
+    document.getElementById("boost-btn").style.display = "block";
+    document.getElementById("fight-btn").style.display = "none";
+  } else {
+    document.getElementById("current-player-pokemon-status").style.display = "none";
+    document.getElementById("target-player-pokemon-status").style.display = "none";
+    document.getElementById("attack-btn").style.display = "none";
+    document.getElementById("boost-btn").style.display = "none";
+    document.getElementById("fight-btn").style.display = "block";
+  }
 }
 
 function attackCommand(currentPlayer, targetPlayer) {
@@ -195,24 +291,20 @@ function attackCommand(currentPlayer, targetPlayer) {
   // generate skill index randomly
   const skillIndex = Math.floor(Math.random() * (battle.currentPlayer.pokemon.skillsVariety.length)) + 0;
 
+  let success = false;
+
   // check if the current pokemon has enough magic point
   if (currentPokemon.currentMagic - currentPokemon.skillsVariety[skillIndex].magicConsume >= 0) {
     currentPokemon.attack(skillIndex, targetPokemon);
     let succesAttackMessage = `${currentPokemon.pokemonName} attack with ${currentPokemon.skillsVariety[skillIndex].skillName} and dealt ${currentPokemon.skillsVariety[skillIndex].attackPower} damage`;
     document.getElementById("battle-log").innerHTML = succesAttackMessage;
+    success = true;
   } else {
-    let failAttackMessage = `${currentPokemon.pokemonName} attack with ${currentPokemon.currentPokemon.skillsVariety[skillIndex].skillName} failed, not enough magic, required:${currentPokemon.skillsVariety[skillIndex].magicConsume}, currentMagic:${currentPokemon.currentMagic}`;
+    let failAttackMessage = `${currentPokemon.pokemonName} attack with ${currentPokemon.skillsVariety[skillIndex].skillName} failed, not enough magic.`;
     document.getElementById("battle-log").innerHTML = failAttackMessage;
+    success = false;
   }
-
-  // check winning condition
-  // if target pokemon health is less than 0 or equal 0 then gameover
-  if (targetPokemon.currentHealth <= 0) {
-    currentGamestate = gamestate.GAMEOVER;
-    setActiveView(currentGamestate);
-    const winningMessage = `Player ${currentPlayer.no} ${currentPlayer.pokemon.pokemonName} win`;
-    document.getElementById("game-over-player-win").innerHTML = winningMessage;
-  }
+  return success;
 }
 
 function getMagicCommand(currentPlayer) {
